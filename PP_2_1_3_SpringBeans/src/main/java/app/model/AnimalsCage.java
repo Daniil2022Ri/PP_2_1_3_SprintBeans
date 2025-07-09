2bfebff96c8312c25947841322b4939bf66dd243
Darkness <mr.deni_2022@mail.ru>
2025-07-09 17:42:14 +0300

    Finish

--- PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
+++ PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
+import java.time.Instant;
+
 @Component
 public class AnimalsCage {
 
@@ -18,12 +20,13 @@ public class AnimalsCage {
         return timer;
     }
 
-
     public void whatAnimalSay() {
+        Instant fixedTime = Instant.now(); //Просто фиксируем время в методе
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(new Timer().getCurrentTime());
         System.out.println("________________________");
     }
 }
+
--- PP_2_1_3_SpringBeans/src/main/java/app/model/Timer.java
+++ PP_2_1_3_SpringBeans/src/main/java/app/model/Timer.java
@@ -2,12 +2,16 @@ package app.model;
 
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDateTime;
+
 @Component
 public class Timer {
-
+    public LocalDateTime getCurrentTime(){
+        return LocalDateTime.now();
+    }
     private Long nanoTime = System.nanoTime();
-
     public Long getTime() {
-        return nanoTime;
+        return System.currentTimeMillis();
     }
 }
+
